3e5c3895f10ecfc12c9d0a6548205e0961d967f2
Levi Santos <levi-sr@outlook.com>
2020-10-19 20:37:29 -0300

    Data Validation

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import * as Yup from 'yup';
 
 import orphanageView from '../views/orphanagesView';
+
 import Orphanage from '../models/Orphanage';
 
 //Model - Representação de uma entidade na aplicação ( dado, usuário, usuário)
@@ -57,7 +59,7 @@ export default {
 
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -66,7 +68,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    };
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      latitude: Yup.number().required(),
+      longitude: Yup.number().required(),
+      about: Yup.string().required().max(300),
+      instructions: Yup.string().required(),
+      opening_hours: Yup.string().required(),
+      open_on_weekends: Yup.boolean().required(),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false
+    });
+
+    const orphanage = orphanagesRepository.create(data);
 
     //salvando no banco
     await orphanagesRepository.save(orphanage);
--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,6 +1,27 @@
 import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidationErrors {
+  // {
+  //   "name": ['obrigatorio', 'minimo de caracteres'],
+  //   "latitude": ['obrigatorio', 'minimo de caracteres'],
+  // }
+
+  [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+  if (error instanceof ValidationError) {
+    let errors: ValidationErrors = {};
+
+    //retornando o erro de uma maneira amigável para o front.
+    error.inner.forEach(error => {
+      errors[error.path] = error.errors;
+    });
+
+    return response.status(400).json({ message: 'Validation fails', errors });
+  }
+
   console.error(error);
 
   return response.status(500).json({ message: 'Internal Server Error.' });
